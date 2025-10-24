2446ad4ee817c3bb26e7dcd7e104e8e05e172767
Shresth Sahai <shresth.sahai@shresths-ltmhdcg.internal.salesforce.com>
2025-10-24 19:41:01 +0530

    gointo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gotuto
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,90 @@
+package main // entry point 
+ 
+import "fmt" //formst package print 
+import "strconv"
+
+var companyName string="ABC"
+
+func main() { //main function 
+
+	// variables 
+	// var studentName string;  // empty string -> memory 
+	// var studentAge int;
+	// var studentGPA float64;
+	// var isEnrolled bool;
+ 	// var studentName string="Alice"; 
+	// fmt.Println(studentName);
+
+	// studentName1 :="Alice"; // only works inside functions 
+
+	// x:=21;
+	// fmt.Print(x);
+
+	// fmt.Println(studentName1);
+
+    // fmt.Println("Hello, Go from VS Code!")
+
+
+
+	// int8 int16 int32 int64 int 
+	// var age uint8 =25; // 0 to 255 
+// float 32 float 64 
+
+// strings -> "", immutable , unicode UTF -9 encode all lang. supported 
+
+// var firstName string="john";
+
+// var greeting string= 'Hello, ';
+
+// first :="Hello"
+// second:="World"
+// result:=first +" "+second
+// fmt.Println(result);
+
+// fmt.Println(first[0])
+// first[0]='g' -> not possible 
+
+// var isStudent bool =true
+
+// Zero values ->  safety 
+// var i int //0
+// var f. float64// 0.0 
+// var s string // ""
+// var b bool //false
+// var p *int // nil pointer
+
+// const pi=3.14 
+// const y int =62
+
+// blank identifier  _  -> ignore values 
+
+
+// num,_ :=strconv.Atoi("123")
+
+// numbers := []int {10,20,20,40}
+
+// for _,value := range numbers{
+// 	fmt.Println(value)
+// }
+
+// scope -> 
+
+// fmt.Println(local)// not accessible
+
+
+// x:=10
+
+// if x>5{
+// 	y:=20
+// 	fmt.Print(x,y) // block scope 
+// }
+
+// fmt.Print(x,y)
+
+
+
+}
+// func greeting(){
+// 	local:="Hi"
+// 	fmt.Println(local)
+// }
